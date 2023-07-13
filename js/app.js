74530d3596feedd1274ec93d96d1b7b368b2d6f2
1JordanWalton1 <dimkevich66455@gmail.com>
2023-07-13 14:42:21 +0300

    content

---
+++ js/app.js
@@ -0,0 +1,98 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(webP.height == 2);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = support === true ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let bodyLockStatus = true;
+    let bodyLockToggle = (delay = 500) => {
+        if (document.documentElement.classList.contains("lock")) bodyUnlock(delay); else bodyLock(delay);
+    };
+    let bodyUnlock = (delay = 500) => {
+        let body = document.querySelector("body");
+        if (bodyLockStatus) {
+            let lock_padding = document.querySelectorAll("[data-lp]");
+            setTimeout((() => {
+                for (let index = 0; index < lock_padding.length; index++) {
+                    const el = lock_padding[index];
+                    el.style.paddingRight = "0px";
+                }
+                body.style.paddingRight = "0px";
+                document.documentElement.classList.remove("lock");
+            }), delay);
+            bodyLockStatus = false;
+            setTimeout((function() {
+                bodyLockStatus = true;
+            }), delay);
+        }
+    };
+    let bodyLock = (delay = 500) => {
+        let body = document.querySelector("body");
+        if (bodyLockStatus) {
+            let lock_padding = document.querySelectorAll("[data-lp]");
+            for (let index = 0; index < lock_padding.length; index++) {
+                const el = lock_padding[index];
+                el.style.paddingRight = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
+            }
+            body.style.paddingRight = window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
+            document.documentElement.classList.add("lock");
+            bodyLockStatus = false;
+            setTimeout((function() {
+                bodyLockStatus = true;
+            }), delay);
+        }
+    };
+    function menuInit() {
+        if (document.querySelector(".icon-menu")) document.addEventListener("click", (function(e) {
+            if (bodyLockStatus && e.target.closest(".icon-menu")) {
+                bodyLockToggle();
+                document.documentElement.classList.toggle("menu-open");
+            }
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    function onEntry(entry) {
+        entry.forEach((change => {
+            if (change.isIntersecting) change.target.classList.add("show");
+        }));
+    }
+    let options = {
+        threshold: [ .5 ]
+    };
+    let observer = new IntersectionObserver(onEntry, options);
+    let script_elements = document.querySelectorAll(".about");
+    for (let elm of script_elements) observer.observe(elm);
+    let cardItems = document.querySelectorAll(".projects__item");
+    for (let elm of cardItems) observer.observe(elm);
+    let skills = document.querySelectorAll(".skills");
+    for (let elm of skills) observer.observe(elm);
+    const btnDarkMode = document.querySelector(".dark-mode-btn");
+    if (localStorage.getItem("darkMode") === "dark") {
+        btnDarkMode.classList.add("dark-mode-btn_active");
+        document.body.classList.add("dark");
+    }
+    btnDarkMode.onclick = function() {
+        btnDarkMode.classList.toggle("dark-mode-btn_active");
+        const isDark = document.body.classList.toggle("dark");
+        if (isDark) localStorage.setItem("darkMode", "dark"); else localStorage.setItem("darkMode", "light");
+    };
+    isWebp();
+    menuInit();
+})();
